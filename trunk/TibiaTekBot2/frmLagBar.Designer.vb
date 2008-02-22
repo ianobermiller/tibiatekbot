1af34ea410e4cd42747e4312c5c2475442568b4c
sonic5.ttb <sonic5.ttb@33ddfa00-593a-0410-b0c9-7b4335ea722e>
2008-02-22 00:43:55 +0000

    -Included missing frmLagBar files (Cameri)

---
+++ trunk/TibiaTekBot2/frmLagBar.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLagBar
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 5000
+        '
+        'Button1
+        '
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(80, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(20, 28)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Tag = "2"
+        Me.Button1.Text = "-"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.ProgressBar1.Location = New System.Drawing.Point(0, 0)
+        Me.ProgressBar1.Maximum = 500
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(80, 11)
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.ProgressBar1.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(0, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "0 ms"
+        '
+        'frmLagBar
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(100, 28)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(300, 28)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(100, 28)
+        Me.Name = "frmLagBar"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "Lag Bar"
+        Me.TopMost = True
+        Me.TransparencyKey = System.Drawing.Color.Magenta
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+End Class
---
+++ trunk/TibiaTekBot2/frmLagBar.vb
@@ -0,0 +1,79 @@
+﻿Public Class frmLagBar
+    Private Declare Function SendMessage Lib "user32" Alias "SendMessageA" (ByVal hWnd As Integer, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As Integer) As Integer
+
+    Private Sub frmLagBar_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        e.Cancel = True
+        Hide()
+    End Sub
+
+    Dim Socket As New Winsock
+    Dim Time As Date
+    Dim Elapsed As Double = 0
+    'Dim MaxElapsed As Integer = 0
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        SyncLock Me
+            Try
+                If Kernel.Client.IsConnected Then
+                    If Socket.GetState <> Winsock.WinsockStates.Closed Then
+                        Socket.Close()
+                        While Socket.GetState <> Winsock.WinsockStates.Closed
+                            Application.DoEvents()
+                        End While
+                    End If
+                    Time = Date.Now
+                    Socket.Connect(Kernel.Client.CharacterListCurrentEntry.WorldIP.ToString, Kernel.Client.CharacterListCurrentEntry.WorldPort)
+                    While Socket.GetState <> Winsock.WinsockStates.Connected
+                        Elapsed = (Date.Now - Time).TotalMilliseconds
+                        If Elapsed > 5000 Then
+                            Label2.Text = (Date.Now - Time).TotalMilliseconds & " ms"
+                            Label2.ForeColor = Drawing.Color.DarkRed
+                            ProgressBar1.Value = ProgressBar1.Maximum
+                            Exit Sub
+                        End If
+                        Application.DoEvents()
+                    End While
+                    Label2.Text = (Date.Now - Time).TotalMilliseconds & " ms"
+                    If Elapsed <= 100 Then
+                        Label2.ForeColor = Drawing.Color.LightGreen
+                    ElseIf Elapsed <= 200 Then
+                        Label2.ForeColor = Drawing.Color.Green
+                    ElseIf Elapsed <= 300 Then
+                        Label2.ForeColor = Drawing.Color.Yellow
+                    ElseIf Elapsed <= 400 Then
+                        Label2.ForeColor = Drawing.Color.Orange
+                    ElseIf Elapsed <= 500 Then
+                        Label2.ForeColor = Drawing.Color.DarkOrange
+                    Else
+                        Label2.ForeColor = Drawing.Color.Red
+                    End If
+                    'ProgressBar1.Maximum = CInt(Fix(Math.Max(ProgressBar1.Maximum, Elapsed)))
+                    ProgressBar1.Value = Math.Min(500, Fix(Elapsed))
+
+                    Socket.Close()
+                    While Socket.GetState <> Winsock.WinsockStates.Closed
+                        Application.DoEvents()
+                    End While
+                Else
+                    Label2.Text = "N/A"
+                    Label2.ForeColor = Drawing.Color.White
+                    ProgressBar1.Value = 0
+                End If
+
+            Catch ex As Exception
+            End Try
+        End SyncLock
+    End Sub
+
+    Private Sub frmLagBar_MouseDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles MyBase.MouseDown, ProgressBar1.MouseDown, Label2.MouseDown
+        If Not My.Computer.Mouse.ButtonsSwapped And e.Button = MouseButtons.Left OrElse _
+           My.Computer.Mouse.ButtonsSwapped And e.Button = MouseButtons.Right Then
+            CType(sender, System.Windows.Forms.Control).Capture = False
+            SendMessage(Handle.ToInt32, &HA1, 2, 0)
+        End If
+    End Sub
+
+    Private Sub Button1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Hide()
+    End Sub
+
+End Class
